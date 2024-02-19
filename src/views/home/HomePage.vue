af85bf9b36a81ccc73fa95e57dc1588e44bd8c05
Chamith Jayaweera <chamithlkml@gmail.com>
2024-02-19 13:32:20 +0000

    Initial file commit

---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="justify-center flex items-center h-screen">
+    <div class="grid grid-cols-3 gap-1/2">
+      <div class="game-square">1</div>
+      <div class="game-square">2</div>
+      <div class="game-square">3</div>
+      <div class="game-square">4</div>
+      <div class="game-square">5</div>
+      <div class="game-square">6</div>
+      <div class="game-square">7</div>
+      <div class="game-square">8</div>
+      <div class="game-square">9</div>
+    </div>
+  </div>
+</template>
+<style scope>
+</style>
